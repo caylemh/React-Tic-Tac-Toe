f2d56e0139e28637389cffb3a6bb44aebf23525f
caylemh <60260455+caylemh@users.noreply.github.com>
2020-04-15 17:39:39 +0200

    Delete app.js

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss'; 
-
-ReactDOM.render(<p>This is my Tic Tac Toe App</p>, document.getElementById('app'));oiler
